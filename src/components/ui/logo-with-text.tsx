ead384600634f3b83417ac721cdfc8934097c50f
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-25 13:05:02 +0300

    feat: adaptive logo

--- src/components/ui/logo-with-text.tsx
+++ src/components/ui/logo-with-text.tsx
@@ -4,7 +4,9 @@ export const Logo = () => {
 	return (
 		<div className="flex gap-2 items-center">
 			<CubeTransparentIcon className="w-8 h-8" />
-			<div className=" inline-block text-2xl font-bold">Реальные проекты</div>
+			<div className=" inline-block text-xl sm:text-2xl font-bold">
+				Реальные проекты
+			</div>
 		</div>
 	);
 };
