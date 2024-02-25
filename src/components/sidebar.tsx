69efd1093c462c32e37f38e9c5197192bc46b0b1
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-25 22:07:02 +0300

    feat: highlight active link in sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
 	CalendarIcon,
 	Cog6ToothIcon,
@@ -7,9 +8,10 @@ import {
 } from "@heroicons/react/24/solid";
 import { Separator } from "./ui/separator";
 import type { ReactElement } from "react";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export const Sidebar = () => {
 	return (
@@ -17,41 +19,36 @@ export const Sidebar = () => {
 			<h4 className="px-4 pt-3 text-xl font-medium">Test project</h4>
 			<div className="grow">
 				<div className="flex flex-col font-medium">
-					<Link href={"/members"}>
-						<MenuItem
-							icon={<UsersIcon className="h-4 w-4" />}
-							title="Участники"
-						/>
-					</Link>
-					<Link href={"/settings"}>
-						<MenuItem
-							icon={<Cog6ToothIcon className="h-4 w-4" />}
-							title="Настройки пространства"
-						/>
-					</Link>
+					<MenuItem
+						icon={<UsersIcon className="h-4 w-4" />}
+						title="Участники"
+						href="/members"
+					/>
+					<MenuItem
+						icon={<Cog6ToothIcon className="h-4 w-4" />}
+						title="Настройки пространства"
+						href="/settings"
+					/>
 				</div>
-				<div className="px-4">
+				<div className="px-4 py-2">
 					<Separator />
 				</div>
 				<div className="flex flex-col font-medium">
-					<Link href={"/dashboard"}>
-						<MenuItem
-							icon={<Squares2X2Icon className="h-4 w-4" />}
-							title="Дэшбоард"
-						/>
-					</Link>
-					<Link href={"/backlog"}>
-						<MenuItem
-							icon={<RectangleStackIcon className="h-4 w-4" />}
-							title="Бэклог"
-						/>
-					</Link>
-					<Link href={"/calendar"}>
-						<MenuItem
-							icon={<CalendarIcon className="h-4 w-4" />}
-							title="Календарь"
-						/>
-					</Link>
+					<MenuItem
+						icon={<Squares2X2Icon className="h-4 w-4" />}
+						title="Дэшбоард"
+						href="/dashboard"
+					/>
+					<MenuItem
+						icon={<RectangleStackIcon className="h-4 w-4" />}
+						title="Бэклог"
+						href="/backlog"
+					/>
+					<MenuItem
+						icon={<CalendarIcon className="h-4 w-4" />}
+						title="Календарь"
+						href="/calendar"
+					/>
 				</div>
 			</div>
 			<div className="sticky bottom-0 bg-background px-4 py-2 text-sm flex justify-between w-full items-center">
@@ -68,21 +65,26 @@ export const Sidebar = () => {
 const MenuItem = ({
 	icon,
 	title,
-	active,
+	href,
 }: {
 	icon: ReactElement;
 	title: string;
-	active?: boolean;
+	href: string;
 }) => {
+	const pathname = usePathname();
+	const activeStyles =
+		pathname === href ? "bg-accent/80 text-accent-foreground/80" : "";
 	return (
-		<div
-			className={cn(
-				active ? "bg-muted" : "",
-				"flex cursor-pointer items-center gap-3 px-4 py-2 text-muted-foreground/70 hover:bg-muted/50",
-			)}
-		>
-			{icon}
-			<span>{title}</span>
-		</div>
+		<Link href={href}>
+			<div
+				className={cn(
+					"flex cursor-pointer items-center gap-3 px-4 py-2 text-muted-foreground/70 hover:bg-muted hover:text-muted-foreground",
+					activeStyles,
+				)}
+			>
+				{icon}
+				<span>{title}</span>
+			</div>
+		</Link>
 	);
 };
