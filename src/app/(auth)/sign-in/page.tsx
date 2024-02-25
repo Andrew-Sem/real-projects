bef14b27d6abb32ce4eca97622111d03e699cc02
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-25 22:37:16 +0300

    feat: logo on sign-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,15 @@
+import { Logo } from "@/components/ui/logos/logo";
 import { SignInForm } from "@/modules/sign-in/components/sign-in-form";
+import Link from "next/link";
 
 export default function SignInPage() {
 	return (
-		<div className="flex flex-col min-h-screen items-center justify-center">
-			<div className="w-full max-w-sm">
+		<div className="flex flex-col items-center">
+			<Link className="rounded-full p-2 mt-10 w-20 h-20 bg-muted" href={"/"}>
+				<Logo className="w-full h-full" />
+			</Link>
+			<p className="font-medium text-2xl mt-4">Реальные проекты</p>
+			<div className="w-full max-w-sm mt-10">
 				<SignInForm />
 			</div>
 		</div>
