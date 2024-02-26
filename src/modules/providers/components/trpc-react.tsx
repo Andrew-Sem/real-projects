d48bc8fe39e33c0030399c71aa5a9e80fd7c8267
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-26 11:02:02 +0300

    refactor: add shared folder

--- src/modules/providers/components/trpc-react.tsx
+++ src/modules/providers/components/trpc-react.tsx
@@ -1,44 +1,44 @@
-"use client"
+"use client";
 
-import { api } from "@/trpc/react"
-import { getUrl, transformer } from "@/trpc/shared"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client"
-import { useState } from "react"
+import { api } from "@/shared/trpc/react";
+import { getUrl, transformer } from "@/shared/trpc/shared";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import { useState } from "react";
 
 export function TRPCReactProvider(props: {
-    children: React.ReactNode
-    cookies: string
+	children: React.ReactNode;
+	cookies: string;
 }) {
-    const [queryClient] = useState(() => new QueryClient())
+	const [queryClient] = useState(() => new QueryClient());
 
-    const [trpcClient] = useState(() =>
-        api.createClient({
-            transformer,
-            links: [
-                loggerLink({
-                    enabled: (op) =>
-                        process.env.NODE_ENV === "development" ||
-                        (op.direction === "down" && op.result instanceof Error),
-                }),
-                unstable_httpBatchStreamLink({
-                    url: getUrl(),
-                    headers() {
-                        return {
-                            cookie: props.cookies,
-                            "x-trpc-source": "react",
-                        }
-                    },
-                }),
-            ],
-        })
-    )
+	const [trpcClient] = useState(() =>
+		api.createClient({
+			transformer,
+			links: [
+				loggerLink({
+					enabled: (op) =>
+						process.env.NODE_ENV === "development" ||
+						(op.direction === "down" && op.result instanceof Error),
+				}),
+				unstable_httpBatchStreamLink({
+					url: getUrl(),
+					headers() {
+						return {
+							cookie: props.cookies,
+							"x-trpc-source": "react",
+						};
+					},
+				}),
+			],
+		}),
+	);
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <api.Provider client={trpcClient} queryClient={queryClient}>
-                {props.children}
-            </api.Provider>
-        </QueryClientProvider>
-    )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<api.Provider client={trpcClient} queryClient={queryClient}>
+				{props.children}
+			</api.Provider>
+		</QueryClientProvider>
+	);
 }
