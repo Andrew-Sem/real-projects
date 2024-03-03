5a1d022902a8269c20895bbab0baed15de531e7f
Andrew-Sem <avvyayvayva@gmail.com>
2024-03-03 21:01:37 +0300

    fix: styles import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,35 @@
-import { Toaster } from "@/shared/components/ui/toaster";
-import { cn } from "@/shared/lib/utils";
-import { Providers } from "@/modules/providers";
-import "@/styles/globals.css";
-import { Inter as FontSans } from "next/font/google";
+import { Toaster } from "@/shared/components/ui/toaster"
+import { cn } from "@/shared/lib/utils"
+import { Providers } from "@/modules/providers"
+import "@/shared/styles/globals.css"
+import { Inter as FontSans } from "next/font/google"
 
 export const metadata = {
-	title: "Реальные проекты",
-	description: "Система для управления студенческими ит-проектами",
-	icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
+    title: "Реальные проекты",
+    description: "Система для управления студенческими ит-проектами",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
+}
 
 export const fontSans = FontSans({
-	subsets: ["latin"],
-	variable: "--font-sans",
-});
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
 
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode
 }) {
-	return (
-		<html lang="ru" suppressHydrationWarning className={fontSans.variable}>
-			<body className={cn("flex min-h-screen flex-col font-sans antialiased")}>
-				<Providers>{children}</Providers>
-				<Toaster />
-			</body>
-		</html>
-	);
+    return (
+        <html lang="ru" suppressHydrationWarning className={fontSans.variable}>
+            <body
+                className={cn(
+                    "flex min-h-screen flex-col font-sans antialiased"
+                )}
+            >
+                <Providers>{children}</Providers>
+                <Toaster />
+            </body>
+        </html>
+    )
 }
