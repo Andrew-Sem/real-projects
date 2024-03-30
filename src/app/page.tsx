1c9d220af9efb3275ea2c76839b35c32e97b3b32
Andrew-Sem <avvyayvayva@gmail.com>
2024-03-30 13:30:05 +0300

    remove mode toggle from landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,6 @@ export default async function Home() {
             <Button variant={"secondary"}>Войти</Button>
           </SignInButton>
         )}
-        <ModeToggle />
       </header>
       <main>
         <section className="container flex flex-col items-center">
